1872e6eb9447864881ff0fc6a0be35568c8ddd92
Cristiano Malanda <91676925+MalandaCS@users.noreply.github.com>
2021-12-13 14:33:29 +0100

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Mlhope Dawson',
+        username: 'Mlhoson',
+        email: 'Mlhoson@email.com'
+    },
+    {
+        id: 2,
+        name: 'Joao Marcos',
+        username: 'JoMar2',
+        email: 'joaomarcos@email.com'
+    },
+    {
+        id: 3,
+        name: 'Kaliyutuka Domain',
+        username: 'DomainKali',
+        email: 'd.kaliyutuka@email.com'
+    },
+    
+    {
+        id: 4,
+        name: 'Andrew Joseph',
+        username: 'JoRider',
+        email: 'a.joseph4@email.com'
+    },
+    {
+        id: 5,
+        name: 'Jesualdo Mungongo',
+        username: 'J.Mungongo',
+        email: 'j.mungongo@email.com'
+    },
+    {
+        id: 6,
+        name: 'Garcia Oyokopenda',
+        username: 'OyokopendaG',
+        email: 'oyokopenda@email.com'
+    },
+    {
+        id: 7,
+        name: 'Andre Jorge',
+        username: 'JorDre',
+        email: 'a.jorge7@email.com'
+    },
+    {
+        id: 8,
+        name: 'Tuzolana Antonio',
+        username: 'TuzoTon',
+        email: 'a.tuzolana@email.com'
+    },
+
+]
